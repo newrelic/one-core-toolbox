95f811c52fdb2aec88c4ce66bcc72df4d249fb20
Daniel Golden <danielgolden90@gmail.com>
2021-10-12 13:55:55 -0400

    feat: some more basic styling

--- code.ts
+++ code.ts
@@ -6,13 +6,12 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 400, title: "One Core Table Creator" });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = async (msg) => {
-  console.log("got this from the UI:", msg)
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
 
