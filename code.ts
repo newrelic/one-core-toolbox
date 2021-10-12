d954e6ae24ec3fd5a5d2102378ab04b48dffc5c8
Daniel Golden <danielgolden90@gmail.com>
2021-10-12 12:14:40 -0400

    feature: header and body cells!

--- code.ts
+++ code.ts
@@ -18,30 +18,41 @@ figma.ui.onmessage = async (msg) => {
 
   if (msg.type === 'create-table') {
     const tableFrame = figma.createFrame()
-    const tableCell = await figma.importComponentByKeyAsync('ce8fa8e8cab07a19f83f4181ac8cbe76093c6bc3')
+    let headerCell = await figma.importComponentByKeyAsync('ce8fa8e8cab07a19f83f4181ac8cbe76093c6bc3')
+    let bodyCell = await figma.importComponentByKeyAsync('8917ab43ec000963ea1d3e1c9c0cdbe23edf0c9e')
     let tableRow = figma.createComponent()
     
-    tableFrame.name = 'Table Frame';
+    tableFrame.name = 'Table';
     tableFrame.counterAxisSizingMode = 'AUTO';
     tableFrame.layoutMode = 'VERTICAL';
     
     tableRow.layoutMode = 'HORIZONTAL';
     tableRow.counterAxisSizingMode = 'AUTO';
-    tableRow.name = 'Table Row';
+    tableRow.name = 'Row';
+
+    headerCell.variantProperties.Type = "Body"
 
     function createTableRow () {
       for (let i = 0; i < msg.cols; i++) {
-        tableRow.appendChild(tableCell.createInstance())
+        tableRow.appendChild(headerCell.createInstance())
       }
 
       tableFrame.appendChild(tableRow)
-
+      
       for (let i = 0; i < msg.rows; i++) {
         tableFrame.appendChild(tableRow.createInstance())
       }
-    }
-    
 
+
+      tableFrame.children.map((row, index) => {
+        if (index > 0) {
+          row.children.map(cell => {
+            cell.swapComponent(bodyCell)
+          })
+        }
+      })
+
+    }
     
     createTableRow()
   }
