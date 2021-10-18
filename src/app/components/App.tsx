cf24f313fbb2f5a53e574e63c6fe2961195e19d4
Daniel Golden <danielgolden90@gmail.com>
2021-10-18 09:09:39 -0400

    fix: multivalue not allowed sometimes

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,121 +1,133 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import '../styles/ui.css';
-import ColumnConfiguration from './ColumnConfiguration';
-import DimensionsSelection from './DimensionsSelection';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "../styles/ui.css";
+import ColumnConfiguration from "./ColumnConfiguration";
+import DimensionsSelection from "./DimensionsSelection";
 
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const [activeScreen, setActiveScreen] = useState('DimensionsSelection')
-    const [activeCol, setActiveCol] = useState(0);
-    const [activeRow, setActiveRow] = useState(0);
-    const [columnConfiguration, setColumnConfiguration] = useState([]);
+  const [activeScreen, setActiveScreen] = useState("DimensionsSelection");
+  const [activeCol, setActiveCol] = useState(0);
+  const [activeRow, setActiveRow] = useState(0);
+  const [columnConfiguration, setColumnConfiguration] = useState([]);
 
-    const handleColumnConfiguration = () => {
-        let columnConfigurationArray = [];
-        
-        if (columnConfiguration.length === 0) {
-            [...Array(activeCol).keys()].map(() => {
-                columnConfigurationArray.push(
-                    {
-                        name: '',
-                        alignment: 'Left',
-                        cellType: 'Text',
-                        multiValue: false
-                    }
-                )
-            })
-        } else if (columnConfiguration.length > 0) {
-            columnConfigurationArray = columnConfiguration;
+  const handleColumnConfiguration = () => {
+    let columnConfigurationArray = [];
 
-            if (columnConfigurationArray.length > activeCol) {
-                const columnsToRemove = columnConfigurationArray.length - activeCol;
-                [...Array(columnsToRemove).keys()].map(() => {
-                    columnConfigurationArray.pop()
-                }) 
-            } else {
-                const columnsToAdd = activeCol - columnConfigurationArray.length; 
-                [...Array(columnsToAdd).keys()].map(() => {
-                    columnConfigurationArray.push(
-                        {
-                            name: '',
-                            alignment: 'Left',
-                            cellType: 'Text',
-                            multiValue: false
-                        }
-                    )
-                })
-            }
-        }
+    if (columnConfiguration.length === 0) {
+      [...Array(activeCol).keys()].map(() => {
+        columnConfigurationArray.push({
+          name: "",
+          alignment: "Left",
+          cellType: "Text",
+          multiValue: false,
+        });
+      });
+    } else if (columnConfiguration.length > 0) {
+      columnConfigurationArray = columnConfiguration;
 
-        setColumnConfiguration(columnConfigurationArray)
+      if (columnConfigurationArray.length > activeCol) {
+        const columnsToRemove = columnConfigurationArray.length - activeCol;
+        [...Array(columnsToRemove).keys()].map(() => {
+          columnConfigurationArray.pop();
+        });
+      } else {
+        const columnsToAdd = activeCol - columnConfigurationArray.length;
+        [...Array(columnsToAdd).keys()].map(() => {
+          columnConfigurationArray.push({
+            name: "",
+            alignment: "Left",
+            cellType: "Text",
+            multiValue: false,
+          });
+        });
+      }
     }
 
-    useEffect(handleColumnConfiguration, [activeCol])
+    setColumnConfiguration(columnConfigurationArray);
+  };
 
-    const handeGridSelectionInputs = (type) => {
-        let { value, min, max } = event.target;
+  useEffect(handleColumnConfiguration, [activeCol]);
 
-        value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+  const handeGridSelectionInputs = (type) => {
+    let { value, min, max } = event.target;
 
-        if (type === 'col') {
-            setActiveCol(parseInt(value))
-        } else if (type === 'row') {
-            setActiveRow(parseInt(value))
-        }
-    };
+    value = Math.max(Number(min), Math.min(Number(max), Number(value)));
 
-    const handleGridSquareClick = (colIndex, rowIndex) => {
-        setActiveCol(colIndex + 1)
-        setActiveRow(rowIndex + 1)
+    if (type === "col") {
+      setActiveCol(parseInt(value));
+    } else if (type === "row") {
+      setActiveRow(parseInt(value));
     }
-    
-    const createTable = () => {
-        parent.postMessage({ 
-            pluginMessage: { 
-              type: 'create-table',
-              cols: activeCol,
-              rows: activeRow,
-              columnConfiguration: columnConfiguration
-            } 
-          }, '*')
+  };
 
-        parent.postMessage({pluginMessage: {type: 'creation-feedback'}}, '*');
-    }
+  const handleGridSquareClick = (colIndex, rowIndex) => {
+    setActiveCol(colIndex + 1);
+    setActiveRow(rowIndex + 1);
+  };
 
-    const goToColumnConfiguration = () => {
-        setActiveScreen('ColumnConfiguration')
-    }
-    
-    const goToDimensionsSelection = () => {
-        setActiveScreen('DimensionsSelection')
-    }
+  const createTable = () => {
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: "create-table",
+          cols: activeCol,
+          rows: activeRow,
+          columnConfiguration: columnConfiguration,
+        },
+      },
+      "*"
+    );
 
-    const handleRouting = () => {
-        if (activeScreen === 'DimensionsSelection') {
-            return (
-                <DimensionsSelection 
-                    handleGridSquareClick={handleGridSquareClick} 
-                    activeCol={activeCol}  
-                    activeRow={activeRow}  
-                    createTable={createTable}
-                    handeGridSelectionInputs={handeGridSelectionInputs}
-                    goToColumnConfiguration={goToColumnConfiguration}
-                />
-            )
-        } else if (activeScreen === 'ColumnConfiguration') {
-            return <ColumnConfiguration
-                     createTable={createTable} 
-                     goToDimensionsSelection={goToDimensionsSelection}
-                     activeCol={activeCol}
-                     columnConfiguration={columnConfiguration}
-                     setColumnConfiguration={setColumnConfiguration}
-                    />
-        }
-    }
+    parent.postMessage({ pluginMessage: { type: "creation-feedback" } }, "*");
+  };
 
-    return handleRouting()
+  const goToColumnConfiguration = () => {
+    setActiveScreen("ColumnConfiguration");
+  };
+
+  const goToDimensionsSelection = () => {
+    setActiveScreen("DimensionsSelection");
+  };
+
+  const handleRouting = () => {
+    if (activeScreen === "DimensionsSelection") {
+      return (
+        <DimensionsSelection
+          handleGridSquareClick={handleGridSquareClick}
+          activeCol={activeCol}
+          activeRow={activeRow}
+          createTable={createTable}
+          handeGridSelectionInputs={handeGridSelectionInputs}
+          goToColumnConfiguration={goToColumnConfiguration}
+        />
+      );
+    } else if (activeScreen === "ColumnConfiguration") {
+      return (
+        <ColumnConfiguration
+          createTable={createTable}
+          goToDimensionsSelection={goToDimensionsSelection}
+          activeCol={activeCol}
+          columnConfiguration={columnConfiguration}
+          setColumnConfiguration={setColumnConfiguration}
+        />
+      );
+    }
+  };
+
+  // Send user data to monitoring tool
+  // React.useEffect(() => {
+  //     // This is how we read messages sent from the plugin controller
+  //     window.onmessage = (event) => {
+  //         const {type, message} = event.data.pluginMessage;
+  //         if (type === 'current-user') {
+  //             console.log(`Figma Says: ${JSON.stringify(message)}`);
+  //         }
+  //     };
+  // }, []);
+
+  return handleRouting();
 };
 
 export default App;
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,101 +1,124 @@
 figma.showUI(__html__, { width: 300, height: 412 });
 
 figma.ui.onmessage = async (msg) => {
-    if (msg.type === 'create-table') {
-        const tableFrame = figma.createFrame()
-        let headerCell = await figma.importComponentByKeyAsync('ce8fa8e8cab07a19f83f4181ac8cbe76093c6bc3')
-        let tableRow = figma.createComponent()
-        let cellFillContainerY = false;
-        
-        await figma.loadFontAsync({ family: "Open Sans", style: "Regular" })
-        
-        tableFrame.name = 'Table';
-        tableFrame.counterAxisSizingMode = 'AUTO';
-        tableFrame.layoutMode = 'VERTICAL';
-        tableFrame.x = figma.viewport.center.x
-        tableFrame.y = figma.viewport.center.y
-        
-        headerCell.variantProperties.Type = "Body";
+  if (msg.type === "create-table") {
+    const tableFrame = figma.createFrame();
+    let headerCell = await figma.importComponentByKeyAsync(
+      "ce8fa8e8cab07a19f83f4181ac8cbe76093c6bc3"
+    );
+    let tableRow = figma.createComponent();
+    let cellFillContainerY = false;
 
-        // if any cell is set to Multi-value, set a variable we'll use later
-        msg.columnConfiguration.find(col => cellFillContainerY = col.multiValue);
+    await figma.loadFontAsync({ family: "Open Sans", style: "Regular" });
 
-        [...Array(msg.rows + 1).keys()].map((rowIndex) => {
-            
-            tableRow.layoutMode = 'HORIZONTAL';
-            tableRow.counterAxisSizingMode = 'AUTO';
-            tableRow.name = 'Row';
-            // tableRow.layoutAlign = 'STRETCH'
-            // tableRow.primaryAxisSizingMode = 'FIXED';
-            
+    tableFrame.name = "Table";
+    tableFrame.counterAxisSizingMode = "AUTO";
+    tableFrame.layoutMode = "VERTICAL";
+    tableFrame.x = figma.viewport.center.x;
+    tableFrame.y = figma.viewport.center.y;
 
-            msg.columnConfiguration.map(async (col, i, arr) => {
-                let { name: colName, alignment: colAlignment, cellType: colCellType, multiValue: colMultiValue } = col;
+    headerCell.variantProperties.Type = "Body";
 
-                colAlignment = colAlignment[0].toUpperCase() + colAlignment.substring(1)
-                colCellType = colCellType[0].toUpperCase() + colCellType.substring(1)
-                colMultiValue = colMultiValue.toString()
-                colMultiValue = colMultiValue[0].toUpperCase() + colMultiValue.substring(1)
+    // if any cell is set to Multi-value, set a variable we'll use later
+    msg.columnConfiguration.find(
+      (col) => (cellFillContainerY = col.multiValue)
+    );
 
-                if (rowIndex === 0) {
-                    let thisHeaderCell = headerCell.createInstance()
-                    let textNodeOfHeaderCell = thisHeaderCell.children[0].children[0]
-                    
-                    thisHeaderCell.name = colName.length > 0 ? colName : 'Header';
+    [...Array(msg.rows + 1).keys()].map((rowIndex) => {
+      tableRow.layoutMode = "HORIZONTAL";
+      tableRow.counterAxisSizingMode = "AUTO";
+      tableRow.name = "Row";
+      // tableRow.layoutAlign = 'STRETCH'
+      // tableRow.primaryAxisSizingMode = 'FIXED';
 
-                    textNodeOfHeaderCell.deleteCharacters(0, textNodeOfHeaderCell.characters.length)
-                    textNodeOfHeaderCell.insertCharacters(0, colName.length > 0 ? colName : 'Header')
-                    thisHeaderCell.setProperties({ "Alignment" : colAlignment})
+      msg.columnConfiguration.map(async (col, i, arr) => {
+        let {
+          name: colName,
+          alignment: colAlignment,
+          cellType: colCellType,
+          multiValue: colMultiValue,
+        } = col;
 
-                    thisHeaderCell.resize(colMultiValue === 'True' ? 120 : thisHeaderCell.width, thisHeaderCell.height)
+        colAlignment =
+          colAlignment[0].toUpperCase() + colAlignment.substring(1);
+        colCellType = colCellType[0].toUpperCase() + colCellType.substring(1);
+        colMultiValue = colMultiValue.toString();
+        colMultiValue =
+          colMultiValue[0].toUpperCase() + colMultiValue.substring(1);
 
-                    // thisHeaderCell.layoutGrow = 1;
+        if (rowIndex === 0) {
+          let thisHeaderCell = headerCell.createInstance();
+          let textNodeOfHeaderCell = thisHeaderCell.children[0].children[0];
 
-                    // if any cell is set to Multi-value then make all of them "fill container" vertically
-                    thisHeaderCell.children[0].layoutGrow = cellFillContainerY ? 1 : 0;
-                    thisHeaderCell.primaryAxisSizingMode = cellFillContainerY ? 'FIXED' : 'AUTO';
-                    
-                    
-                    tableRow.appendChild(thisHeaderCell)
-                } 
-            }) 
+          thisHeaderCell.name = colName.length > 0 ? colName : "Header";
 
-            if (rowIndex === 0 ) {
-                tableFrame.appendChild(tableRow)
-            } else {
-                let thisTableRow = tableRow.createInstance();
+          textNodeOfHeaderCell.deleteCharacters(
+            0,
+            textNodeOfHeaderCell.characters.length
+          );
+          textNodeOfHeaderCell.insertCharacters(
+            0,
+            colName.length > 0 ? colName : "Header"
+          );
+          thisHeaderCell.setProperties({ Alignment: colAlignment });
 
-                // thisTableRow.layoutAlign = 'STRETCH'
-                // thisTableRow.primaryAxisSizingMode = 'FIXED';
-                
-                thisTableRow.children.map((cell, index) => {
-                    let { cellType: colCellType, multiValue: colMultiValue } = msg.columnConfiguration[index];
-    
-                    colCellType = colCellType[0].toUpperCase() + colCellType.substring(1)
-                    colMultiValue = colMultiValue.toString()
-                    colMultiValue = colMultiValue[0].toUpperCase() + colMultiValue.substring(1)
+          thisHeaderCell.resize(
+            colMultiValue === "True" ? 120 : thisHeaderCell.width,
+            thisHeaderCell.height
+          );
 
-                    cell.name === 'Header' ? cell.name = 'Cell' : null;
-                    cell.setProperties({'Type': 'Body'})
-                    cell.children[0].children[0].setProperties({
-                        "Type": colCellType,
-                        "Multi-value": colMultiValue,
-                    })
+          // thisHeaderCell.layoutGrow = 1;
 
-                    // Because cells can be reset here as they're replaced with another 
-                    // component (variants), we again set the fill container setting if
-                    // any of the columns is set to "multi value"
-                    cell.children[0].layoutGrow = cellFillContainerY ? 1 : 0;
-                    cell.primaryAxisSizingMode = cellFillContainerY ? 'FIXED' : 'AUTO';
-                })
+          // if any cell is set to Multi-value then make all of them "fill container" vertically
+          thisHeaderCell.children[0].layoutGrow = cellFillContainerY ? 1 : 0;
+          thisHeaderCell.primaryAxisSizingMode = cellFillContainerY
+            ? "FIXED"
+            : "AUTO";
 
-                tableFrame.appendChild(thisTableRow)
-            }
-        })
+          tableRow.appendChild(thisHeaderCell);
+        }
+      });
 
-        figma.notify('Table created ✅');
-        
-        figma.closePlugin();
-    }
-    
+      if (rowIndex === 0) {
+        tableFrame.appendChild(tableRow);
+      } else {
+        let thisTableRow = tableRow.createInstance();
+
+        // thisTableRow.layoutAlign = 'STRETCH'
+        // thisTableRow.primaryAxisSizingMode = 'FIXED';
+
+        thisTableRow.children.map((cell, index) => {
+          let { cellType: colCellType, multiValue: colMultiValue } =
+            msg.columnConfiguration[index];
+
+          colCellType = colCellType[0].toUpperCase() + colCellType.substring(1);
+          colMultiValue = colMultiValue.toString();
+          colMultiValue =
+            colMultiValue[0].toUpperCase() + colMultiValue.substring(1);
+
+          cell.name === "Header" ? (cell.name = "Cell") : null;
+          cell.setProperties({ Type: "Body" });
+          cell.children[0].children[0].setProperties({
+            Type: colCellType,
+            "Multi-value": colMultiValue,
+          });
+
+          // Because cells can be reset here as they're replaced with another
+          // component (variants), we again set the fill container setting if
+          // any of the columns is set to "multi value"
+          cell.children[0].layoutGrow = cellFillContainerY ? 1 : 0;
+          cell.primaryAxisSizingMode = cellFillContainerY ? "FIXED" : "AUTO";
+        });
+
+        tableFrame.appendChild(thisTableRow);
+      }
+    });
+
+    let userData = figma.currentUser;
+    figma.ui.postMessage({ type: "current-user", message: userData });
+
+    figma.notify("Table created ✅");
+
+    figma.closePlugin();
+  }
 };
