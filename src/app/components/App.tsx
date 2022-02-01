a62018b2d77f05ee9989f910815ec5a71bd1cef5
danielgolden <danielgolden90@gmail.com>
2022-02-01 10:43:37 -0500

    chore: some prettier formatting

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,154 +1,154 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import '../styles/ui.css';
-import ColumnConfiguration from './ColumnConfiguration';
-import DimensionsSelection from './DimensionsSelection';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "../styles/ui.css";
+import ColumnConfiguration from "./ColumnConfiguration";
+import DimensionsSelection from "./DimensionsSelection";
 
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const [activeScreen, setActiveScreen] = useState('DimensionsSelection')
-    const [activeCol, setActiveCol] = useState(0);
-    const [activeRow, setActiveRow] = useState(0);
-    const [columnConfiguration, setColumnConfiguration] = useState([]);
+  const [activeScreen, setActiveScreen] = useState("DimensionsSelection");
+  const [activeCol, setActiveCol] = useState(0);
+  const [activeRow, setActiveRow] = useState(0);
+  const [columnConfiguration, setColumnConfiguration] = useState([]);
 
-    const handleColumnConfiguration = () => {
-        let columnConfigurationArray = [];
-        
-        if (columnConfiguration.length === 0) {
-            [...Array(activeCol).keys()].map(() => {
-                columnConfigurationArray.push(
-                    {
-                        name: '',
-                        alignment: 'Left',
-                        cellType: 'Text',
-                        multiValue: false
-                    }
-                )
-            })
-        } else if (columnConfiguration.length > 0) {
-            columnConfigurationArray = columnConfiguration;
+  const handleColumnConfiguration = () => {
+    let columnConfigurationArray = [];
 
-            if (columnConfigurationArray.length > activeCol) {
-                const columnsToRemove = columnConfigurationArray.length - activeCol;
-                [...Array(columnsToRemove).keys()].map(() => {
-                    columnConfigurationArray.pop()
-                }) 
-            } else {
-                const columnsToAdd = activeCol - columnConfigurationArray.length; 
-                [...Array(columnsToAdd).keys()].map(() => {
-                    columnConfigurationArray.push(
-                        {
-                            name: '',
-                            alignment: 'Left',
-                            cellType: 'Text',
-                            multiValue: false
-                        }
-                    )
-                })
-            }
-        }
+    if (columnConfiguration.length === 0) {
+      [...Array(activeCol).keys()].map(() => {
+        columnConfigurationArray.push({
+          name: "",
+          alignment: "Left",
+          cellType: "Text",
+          multiValue: false,
+        });
+      });
+    } else if (columnConfiguration.length > 0) {
+      columnConfigurationArray = columnConfiguration;
 
-        setColumnConfiguration(columnConfigurationArray)
+      if (columnConfigurationArray.length > activeCol) {
+        const columnsToRemove = columnConfigurationArray.length - activeCol;
+        [...Array(columnsToRemove).keys()].map(() => {
+          columnConfigurationArray.pop();
+        });
+      } else {
+        const columnsToAdd = activeCol - columnConfigurationArray.length;
+        [...Array(columnsToAdd).keys()].map(() => {
+          columnConfigurationArray.push({
+            name: "",
+            alignment: "Left",
+            cellType: "Text",
+            multiValue: false,
+          });
+        });
+      }
     }
 
-    useEffect(handleColumnConfiguration, [activeCol])
+    setColumnConfiguration(columnConfigurationArray);
+  };
 
-    const handeGridSelectionInputs = (type) => {
-        let { value, min, max } = event.target;
+  useEffect(handleColumnConfiguration, [activeCol]);
 
-        value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+  const handeGridSelectionInputs = (type) => {
+    let { value, min, max } = event.target;
 
-        if (type === 'col') {
-            setActiveCol(parseInt(value))
-        } else if (type === 'row') {
-            setActiveRow(parseInt(value))
-        }
+    value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+
+    if (type === "col") {
+      setActiveCol(parseInt(value));
+    } else if (type === "row") {
+      setActiveRow(parseInt(value));
+    }
+  };
+
+  const handleGridSquareClick = (colIndex, rowIndex) => {
+    setActiveCol(colIndex + 1);
+    setActiveRow(rowIndex + 1);
+  };
+
+  const createTable = () => {
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: "create-table",
+          cols: activeCol,
+          rows: activeRow,
+          columnConfiguration: columnConfiguration,
+        },
+      },
+      "*"
+    );
+
+    parent.postMessage({ pluginMessage: { type: "creation-feedback" } }, "*");
+  };
+
+  const goToColumnConfiguration = () => {
+    setActiveScreen("ColumnConfiguration");
+  };
+
+  const goToDimensionsSelection = () => {
+    setActiveScreen("DimensionsSelection");
+  };
+
+  const renderTableCreator = () => {
+    if (activeScreen === "DimensionsSelection") {
+      return (
+        <DimensionsSelection
+          handleGridSquareClick={handleGridSquareClick}
+          activeCol={activeCol}
+          activeRow={activeRow}
+          createTable={createTable}
+          handeGridSelectionInputs={handeGridSelectionInputs}
+          goToColumnConfiguration={goToColumnConfiguration}
+        />
+      );
+    } else if (activeScreen === "ColumnConfiguration") {
+      return (
+        <ColumnConfiguration
+          createTable={createTable}
+          goToDimensionsSelection={goToDimensionsSelection}
+          activeCol={activeCol}
+          columnConfiguration={columnConfiguration}
+          setColumnConfiguration={setColumnConfiguration}
+        />
+      );
+    }
+  };
+
+  // Send user data to monitoring tool
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = (event) => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === "table-created") {
+        newrelic.addPageAction("tableCreated", {
+          "User ID": message.userData.id,
+          "User Name": message.userData.name,
+          "User Avatar": message.userData.photoUrl,
+          "Session ID": message.userData.sessionId,
+          "Column count": message.columnCount,
+          "Row count": message.rowCount,
+          "Column Configuration": message.columnConfiguration,
+        });
+      }
     };
+  }, []);
 
-    const handleGridSquareClick = (colIndex, rowIndex) => {
-        setActiveCol(colIndex + 1)
-        setActiveRow(rowIndex + 1)
-    }
-    
-    const createTable = () => {
-        parent.postMessage({ 
-            pluginMessage: { 
-              type: 'create-table',
-              cols: activeCol,
-              rows: activeRow,
-              columnConfiguration: columnConfiguration
-            } 
-          }, '*')
-
-        parent.postMessage({pluginMessage: {type: 'creation-feedback'}}, '*');
-    }
-
-    const goToColumnConfiguration = () => {
-        setActiveScreen('ColumnConfiguration')
-    }
-    
-    const goToDimensionsSelection = () => {
-        setActiveScreen('DimensionsSelection')
-    }
-
-    const handleRouting = () => {
-        if (activeScreen === 'DimensionsSelection') {
-            return (
-                <DimensionsSelection 
-                    handleGridSquareClick={handleGridSquareClick} 
-                    activeCol={activeCol}  
-                    activeRow={activeRow}  
-                    createTable={createTable}
-                    handeGridSelectionInputs={handeGridSelectionInputs}
-                    goToColumnConfiguration={goToColumnConfiguration}
-                />
-            )
-        } else if (activeScreen === 'ColumnConfiguration') {
-            return <ColumnConfiguration
-                     createTable={createTable} 
-                     goToDimensionsSelection={goToDimensionsSelection}
-                     activeCol={activeCol}
-                     columnConfiguration={columnConfiguration}
-                     setColumnConfiguration={setColumnConfiguration}
-                    />
-        }
-    }
-
-    // Send user data to monitoring tool
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const {type, message} = event.data.pluginMessage;
-            if (type === 'table-created') {
-                newrelic.addPageAction(
-                    'tableCreated', {
-                        'User ID': message.userData.id,
-                        'User Name': message.userData.name,
-                        'User Avatar': message.userData.photoUrl,
-                        'Session ID': message.userData.sessionId,
-                        'Column count': message.columnCount,
-                        'Row count': message.rowCount,
-                        'Column Configuration': message.columnConfiguration
-                    } 
-                );
-            }
-        };
-    }, []);
-
-    return (
-        <>
-            {
-                <nav className="tab-navigation">
-                    <ul className="tab-navigation-tabs">
-                        <li className="tab-navigation-tab active">Table creator</li>
-                        <li className="tab-navigation-tab">More coming soon...</li>
-                    </ul>
-                </nav>
-            }  
-            {handleRouting()}
-        </>
-    )
+  return (
+    <>
+      {
+        <nav className="tab-navigation">
+          <ul className="tab-navigation-tabs">
+            <li className="tab-navigation-tab active">Table creator</li>
+            <li className="tab-navigation-tab">Language linter</li>
+            <li className="tab-navigation-tab">More coming soon...</li>
+          </ul>
+        </nav>
+      }
+      {renderTableCreator()}
+    </>
+  );
 };
 
 export default App;
