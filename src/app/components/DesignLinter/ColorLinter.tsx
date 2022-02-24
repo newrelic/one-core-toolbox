5c78865463e2100e06217365eeec480e03f69a95
Daniel Golden <danielgolden90@gmail.com>
2022-02-24 08:59:53 -0500

    chore: move ColorTile to component

--- src/app/components/DesignLinter/ColorLinter.tsx
+++ src/app/components/DesignLinter/ColorLinter.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
+import ColorTile from './ColorTile'
 require("babel-polyfill");
 import "../../styles/ui.css";
 
@@ -18,118 +19,24 @@ const ColorLinter = () => {
       if (type === 'color-stats') {
         setSelectionColorStats(message);
       }
-
-      // if (type === "selection-change") {
-      //   // Filter out text layers that have no suggestions
-      //   setSelectionColorStats(message)
-      // }
     };
   }, []);
-
-  const handleColorTileClick = (layerId: string) => {
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: 'select-layer',
-          layerId: layerId,
-        },
-      },
-      '*'
-    );
-  };
 
   const renderColorIssues = () => {
     const colorsWithIssues = selectionColorStats?.colorsNotUsingOneCoreColorStyle;
 
-    const truncateLayerName = (layerName: string): string => {
-      if (layerName.length > 30) {
-        return layerName.substring(0, 27) + '...';
-      }
-
-      return layerName;
-    };
-
     if (colorsWithIssues) {
       return colorsWithIssues.map((color, index) => {
-        const {
-          // colorId,
-          layerId,
-          layerName,
-          // layerType,
-          // colorStyleId,
-          // hasColorStyle,
-          // visible,
-          colorType,
-          colorInHex,
-        } = color;
-
-        return (
-          <li className="color-tile-container" key={index} onClick={() => handleColorTileClick(layerId)}>
-
-            <div className="color-tile-header">
-              <div className="color-tile-title-section">
-                <h4 className="color-tile-heading">Missing One Core Color style</h4>
-                <button className="btn-color-tile-menu">Options</button>
-              </div>
-
-              <ul className="color-tile-meta-list">
-                <li className="color-tile-meta-list-item color-tile-meta-color">
-                  <span style={{backgroundColor: colorInHex}} className="color-tile-color-sample"></span>
-                  {colorInHex}
-                </li>
-                <li className="color-tile-meta-list-item color-tile-meta-layer-name">
-                  Layer: {truncateLayerName(layerName)}
-                </li>
-                <li className="color-tile-meta-list-item">Type: {colorType}</li>
-              </ul>
-            </div>
-
-            <article className="suggested-color-styles-container">
-              <div className="suggested-color-style-header">
-                <h5 className="suggested-color-style-heading">Suggested color styles</h5>
-                <button className="btn-suggested-color-style-help">Help</button>
-              </div>
-
-              <ul className="suggested-color-style-list">
-                <li className="suggested-color-style-list-item">
-                  <span 
-                    className="suggested-color-style-sample"
-                    style={{backgroundColor: `#f9f9f9`}}
-                  ></span>
-                  Text / Color / Token / Name
-                </li>
-                <li className="suggested-color-style-list-item">
-                  <span 
-                    className="suggested-color-style-sample"
-                    style={{backgroundColor: `#f9f9f9`}}
-                  ></span>
-                  Text / Color / Token / Name
-                </li>
-                <li className="suggested-color-style-list-item">
-                  <span 
-                    className="suggested-color-style-sample"
-                    style={{backgroundColor: `#f9f9f9`}}
-                  ></span>
-                  Text / Color / Token / Name
-                </li>
-                <li className="suggested-color-style-list-item">
-                  <span 
-                    className="suggested-color-style-sample"
-                    style={{backgroundColor: `#f9f9f9`}}
-                  ></span>
-                  Text / Color / Token / Name
-                </li>
-              </ul>
-            </article>
-          </li>
-        );
+        return <ColorTile colorData={color} key={index} />
       });
     }
   };
 
   return (
     <>
-      <ul className="color-tiles-container">{renderColorIssues()}</ul>
+      <ul className="color-tiles-container">
+        {renderColorIssues()}
+      </ul>
     </>
   );
 };
---
+++ src/app/components/DesignLinter/ColorTile.tsx
@@ -0,0 +1,123 @@
+import * as React from "react";
+import { useState } from "react";
+require("babel-polyfill");
+import "../../styles/ui.css";
+
+declare function require(path: string): any;
+
+interface colorData {
+  layerId: string,
+  layerName: string,
+  colorType: string,
+  colorInHex: string
+}
+
+interface props {
+  colorData: colorData
+  key: number,
+}
+
+const ColorTile = (props: props) => {
+	const {
+    // colorId,
+    layerId,
+    layerName,
+    // layerType,
+    // colorStyleId,
+    // hasColorStyle,
+    // visible,
+    colorType,
+    colorInHex,
+  } = props.colorData;
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleColorTileClick = (layerId: string) => {
+    setIsExpanded(!isExpanded)
+  	// Tell the controller to select and zoom into it
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: 'select-layer',
+          layerId: layerId,
+        },
+      },
+      '*'
+    );
+  };
+
+
+  const truncateLayerName = (layerName: string): string => {
+    if (layerName.length > 30) {
+      return layerName.substring(0, 27) + '...';
+    }
+
+    return layerName;
+  };
+
+  return (
+    <li 
+    	className={`color-tile-container ${isExpanded ? 'expanded' : ''}`}
+    	key={props.key} 
+    	onClick={() => handleColorTileClick(layerId)}
+    >
+      <div className="color-tile-header">
+        <div className="color-tile-title-section">
+          <h4 className="color-tile-heading">Missing One Core Color style</h4>
+          <button className="btn-color-tile-menu">Options</button>
+        </div>
+
+        <ul className="color-tile-meta-list">
+          <li className="color-tile-meta-list-item color-tile-meta-color">
+            <span style={{backgroundColor: colorInHex}} className="color-tile-color-sample"></span>
+            {colorInHex}
+          </li>
+          <li className="color-tile-meta-list-item color-tile-meta-layer-name">
+            Layer: {truncateLayerName(layerName)}
+          </li>
+          <li className="color-tile-meta-list-item">Type: {colorType}</li>
+        </ul>
+      </div>
+
+      <article className="suggested-color-styles-container">
+        <div className="suggested-color-style-header">
+          <h5 className="suggested-color-style-heading">Suggested color styles</h5>
+          <button className="btn-suggested-color-style-help">Help</button>
+        </div>
+
+        <ul className="suggested-color-style-list">
+          <li className="suggested-color-style-list-item">
+            <span 
+              className="suggested-color-style-sample"
+              style={{backgroundColor: `#f9f9f9`}}
+            ></span>
+            Text / Color / Token / Name
+          </li>
+          <li className="suggested-color-style-list-item">
+            <span 
+              className="suggested-color-style-sample"
+              style={{backgroundColor: `#f9f9f9`}}
+            ></span>
+            Text / Color / Token / Name
+          </li>
+          <li className="suggested-color-style-list-item">
+            <span 
+              className="suggested-color-style-sample"
+              style={{backgroundColor: `#f9f9f9`}}
+            ></span>
+            Text / Color / Token / Name
+          </li>
+          <li className="suggested-color-style-list-item">
+            <span 
+              className="suggested-color-style-sample"
+              style={{backgroundColor: `#f9f9f9`}}
+            ></span>
+            Text / Color / Token / Name
+          </li>
+        </ul>
+      </article>
+    </li>
+  );
+};
+
+export default ColorTile;
