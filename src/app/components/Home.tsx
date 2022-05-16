a56c3315fb8120d6ab6c97578380edbe51719928
danielgolden <danielgolden90@gmail.com>
2022-05-16 13:24:27 -0400

    feat(Dark Mode): Adds support for Figma dark mode

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
 import "../styles/ui.css";
-import IconTableCreator from "../assets/icon-table-creator.svg";
-import IconDesignLinter from "../assets/icon-design-linter.svg";
-import IconThemeSwitcher from "../assets/icon-theme-switcher.svg";
 
 const Home = (props) => {
   const { setActivePlugin } = props;
 
   const PluginTile = (props) => {
-    const { heading, description, onClick, icon } = props;
+    const { heading, description, onClick, pluginName } = props;
+
+    const tileClasses = {
+      tableCreator: "table-creator",
+      themeSwitcher: "theme-switcher",
+      designLinter: "design-linter",
+    };
 
     return (
-      <div className="plugin-tile-container" onClick={() => onClick()}>
-        <img src={icon} alt={`${heading} icon`} />
+      <div
+        className={`plugin-tile-container ${tileClasses[pluginName]}`}
+        onClick={() => onClick()}
+      >
+        <div className="plugin-tile-icon" aria-hidden="true"></div>
         <div className="plugin-tile-copy">
           <h3 className="plugin-tile-heading">{heading}</h3>
           <p className="plugin-tile-description">{description}</p>
@@ -24,21 +30,21 @@ const Home = (props) => {
   return (
     <div className="home-container">
       <PluginTile
+        pluginName="tableCreator"
         heading="Table creator"
         description="Create One Core tables with your cell and column settings."
-        icon={IconTableCreator}
         onClick={() => setActivePlugin("table-creator")}
       />
       <PluginTile
+        pluginName="themeSwitcher"
         heading="Theme switcher"
         description="A dark mode or light mode setting for your layers"
-        icon={IconThemeSwitcher}
         onClick={() => setActivePlugin("theme-switcher")}
       />
       <PluginTile
+        pluginName="designLinter"
         heading="Design linter"
         description="Checks your layers for color styles, and soon will check for language too."
-        icon={IconDesignLinter}
         onClick={() => setActivePlugin("color-linter")}
       />
 
